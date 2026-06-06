66348ecbe7e35e1c48bdb0277d1aa6e38a4822bd
chetanuchiha16 <chetan16ck@gmail.com>
2026-06-06 18:07:42 +0530

    added a goroutine with cancellation using waitgroup

--- workers/trial4/main.go
+++ workers/trial4/main.go
@@ -1,2 +1,27 @@
 package main
+
 /// add a concurrency with context for cancellation
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	var wg sync.WaitGroup
+
+	defer cancel()
+	wg.Add(1)
+	go func(ctx context.Context, wg *sync.WaitGroup) {
+		defer wg.Done()
+		select {
+		case <-time.After(4 * time.Second):
+			fmt.Println("done within time")
+		case <-ctx.Done():
+			fmt.Println("time out")
+		}
+	}(ctx, &wg)
+	wg.Wait()
+}
